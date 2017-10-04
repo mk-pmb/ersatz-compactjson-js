8a4d3eda11393307352e649f8b4addffd0985cca
mk-pmb <mk-pmb@users.noreply.github.com>
2017-10-04 03:05:05 +0200

    v0.1.4: cast dark object in object (in array) magic

--- shim.compactjson.js
+++ shim.compactjson.js
@@ -7,19 +7,27 @@ module.exports = (function () {
 
   cj = function cj(x, opt) {
     opt = (opt || false);
+    var rx;
 
     x = JSON.stringify(x, (opt.serializer || null), 2);
     x = x.replace(/(\n\s*|$)/g, '\r$1\r');
 
     // merge opening brackets
+    if (opt.width === 0) {
+      rx = /(\[|: \{)\r(\n +)\r(?:(\{|\[)\r\n( )\s*|)("[ -\uFFFF]+)\r/g;
+      // ^-- object in object (in array) magic
+      x = x.replace(rx, '$1$2$3$4$5');
+    }
     x = x.replace(/(\{|\[)\r\n +\r/g, '$1 ');
 
     // merge closing brackets
     x = x.replace(/\r\n *\r(\]|\})/g, ' $1');
 
     // merge flat value lines
-    x = x.replace(/,\r(\n\s*)\r((?:"\w+":|) *(?:\[|\{))/g, ',$1$2');
-    x = x.replace(/(["\*-Z_-z],)[\r\n]+ *(?=\r)/g, '$1 ');
+    if (opt.width !== 0) {
+      x = x.replace(/,\r(\n\s*)\r((?:"\w+":|) *(?:\[|\{))/g, ',$1$2');
+      x = x.replace(/(["\*-Z_-z],)[\r\n]+ *(?=\r)/g, '$1 ');
+    }
 
     if (opt.ersLW) {
       x = opt.ersLW(x, { width: opt.width, indent: '  ',
